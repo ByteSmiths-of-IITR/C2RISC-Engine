f8a41b9ec73cbe18f026d45c513798468d841931
Vineet Kumar <113177292+Vineet011235@users.noreply.github.com>
2025-02-04 17:01:32 +0530

    resolved wrong unclosed string error reporting

--- src/Lexer/input.cpp
+++ src/Lexer/input.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 
-using namespace std;
-
-#define MAX 100 // This is a macro
-
 int main() {
-    int num1, num2, sum;
-"
-    int a = 5;
-    int b = 10;
-
-    sum = num1 + num2;
-
-/*
+    std::cout << "Hello, World!" << std::endl;
     return 0;
+"    "
+
+    /** */
 }
