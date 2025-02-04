dea44566e59c56950780a3782046082776d33d70
Vineet Kumar <113177292+Vineet011235@users.noreply.github.com>
2025-02-04 16:17:56 +0530

    upgraded for c++ lexical analysis | Basic Error Handling

---
+++ src/Lexer/input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 100 // This is a macro
+
+int main() {
+    int num1, num2, sum;
+"
+    int a = 5;
+    int b = 10;
+
+    sum = num1 + num2;
+
+/*
+    return 0;
+}
